fb004c17888356f8140d58133adfde95513073ac
Gururaj Chadaga <gururajchadaga@Gururajs-MacBook-Air.local>
2022-10-22 12:00:15 +0530

    Added nodemon, refactor

--- understanding_the_basics/playground.js
+++ understanding_the_basics/playground.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
-console.log('hello from nodejs')
+const fs = require('fs');
+console.log('hello from nodejs');
 
-fs.writeFileSync('hello.txt', 'Hello to file from nodejs')
+fs.writeFileSync('hello.txt', 'Hello to file from nodejs');

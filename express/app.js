780560c8546e82894d67c23aa441ecd3ee22934a
Gururaj Chadaga <gururajchadaga@Gururajs-MacBook-Air.local>
2022-10-22 16:17:45 +0530

    handling routes and 404

--- express/app.js
+++ express/app.js
@@ -1,8 +1,10 @@
 const http = require('http');
 
 const express = require('express');
-
 const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 // express() returns a function that initialises an object that handles heavy lifting behing the scene
 const app = express();
@@ -24,20 +26,25 @@ app.use('/', (req, res, next) => {
   next();
 });
 
-app.use('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>'
-  );
-});
+// app.use('/add-product', (req, res, next) => {
+//   res.send(
+//     '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>'
+//   );
+// });
 
-// post() is same as use() but only works for post requests
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
+// // post() is same as use() but only works for post requests
+// app.post('/product', (req, res, next) => {
+//   console.log(req.body);
+//   res.redirect('/');
+// });
 
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from express</h1>');
+// app.use('/', (req, res, next) => {
+//   res.send('<h1>Hello from express</h1>');
+// });
+app.use(adminRoutes);
+app.use(shopRoutes);
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Not found</h1>');
 });
 
 // app is also a valid request handler

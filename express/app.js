373404c06d007d2c498ff3eccee1ce97368d2d7e
Gururaj Chadaga <gururajchadaga@Gururajs-MacBook-Air.local>
2022-10-22 12:33:02 +0530

    Working with middlewares

--- express/app.js
+++ express/app.js
@@ -2,8 +2,24 @@ const http = require('http');
 
 const express = require('express');
 
-const app = express(); // express() returns a function that initialises an object that handles heavy lifting behing the scene
+// express() returns a function that initialises an object that handles heavy lifting behing the scene
+const app = express();
 
-const server = http.createServer(app); // app is also a valid request handler
+// use() is provided by express and allows to add middleware.
+// fn passed to use() is executed for every incoming request
+// next is a function that allows to continue to the next middleware
+app.use((req, res, next) => {
+  console.log('In the middleware');
+  next();
+});
 
-server.listen(3000);
+app.use((req, res, next) => {
+  console.log('In another middleware');
+  res.send('<h1>Hello from express</h1>');
+});
+
+// app is also a valid request handler
+// const server = http.createServer(app);
+// server.listen(3000);
+
+app.listen(3000);

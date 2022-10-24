7de9f606d92d13f897525412baad4861d8d152c4
Gururaj Chadaga <gururajchadaga@Gururajs-MacBook-Air.local>
2022-10-24 10:19:20 +0530

    Code refactor and cleanup

---
+++ 3_MVC/routes/shop.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', { docTitle: 'Shop', active: 'shop', prods: products });
+});
+
+module.exports = router;

4c8b1f71d5bbbb6f66cf9312028d7cd96ae77f01
Gururaj Chadaga <gururajchadaga@Gururajs-MacBook-Air.local>
2022-10-24 12:08:34 +0530

    Added models

--- 3_MVC/controllers/products.js
+++ 3_MVC/controllers/products.js
@@ -1,16 +1,16 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProducts = (req, res, next) => {
   res.render('add-product', { docTitle: 'Add Product', active: 'add-product' });
 };
 
 exports.postAddProducts = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll();
   res.render('shop', { docTitle: 'Shop', active: 'shop', prods: products });
 };
-
-exports.products = products;
